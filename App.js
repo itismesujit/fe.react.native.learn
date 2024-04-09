7f881cf35237e5ba5e2053e06ef2ab9bac0c527a
itismesujit <sjsharma.pro@gmail.com>
2024-04-09 12:05:41 +0530

    ex.Image from react-native

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable } from "react-native";
 
 
+
 export default function App(){
 
+  const logoImage = require('./assets/adaptive-icon.png');
+  
   return(
     <View style={{flex:1, backgroundColor:'plum', padding:60}}>
       <Button 
       title="Click" 
       onPress={()=> alert('hello sujit')}
       color="midnightblue"
-      disabled
       />
+
+      <Image source={logoImage} style={{width:300, height:300}}/>
+      <Image source={{uri:'https://picsum.photos/300'}} style={{width:300, height:300}}/>
     </View>
   )
 }
