37475c4a7bcc4b2a342c251aa690a8a97cd07378
itismesujit <sjsharma.pro@gmail.com>
2024-04-09 11:57:11 +0530

    example.button from react-native

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+export default function App(){
+
+  return(
+    <View style={{flex:1, backgroundColor:'plum', padding:60}}>
+      <Button 
+      title="Click" 
+      onPress={()=> alert('hello sujit')}
+      color="midnightblue"
+      disabled
+      />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
